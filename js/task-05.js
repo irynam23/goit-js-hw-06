0689d3d786bee5972ceb79a01367a5c8305e1fba
Iryna <Iryna@MacBook-Air-MacBook.local>
2022-12-23 14:16:10 +0200

    update

--- js/task-05.js
+++ js/task-05.js
@@ -3,9 +3,5 @@ const input = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
 input.addEventListener("input", (event) => {
-    if (event.currentTarget.value === "") {
-        output.textContent = "Anonymous"
-        return
-    }
-    output.textContent = event.currentTarget.value;
+    !event.currentTarget.value ? output.textContent = "Anonymous" : output.textContent = event.currentTarget.value;
 })
--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,11 @@
 const input = document.querySelector('#font-size-control');
 const text = document.querySelector('#text');
+text.style.fontSize = input.value + 'px'
+
 
 input.addEventListener('input', (event) => {
     const value = event.currentTarget.value
     text.style.fontSize = value + 'px'
+
+
 })
